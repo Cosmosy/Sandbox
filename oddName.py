b42551e46450453eced048862e1ad88da5b1c8db
Cosmosy <william.robinson@my.jcu.edu.au>
2016-08-09 13:53:59 +1000

    Modified: oddName

--- oddName.py
+++ oddName.py
@@ -1,3 +1,11 @@
 """
-William Robinson -
+William Robinson - Asks user for name and displays every second letter
 """
+
+name = input("Please enter your name: ")
+while name == "":
+    print("Invalid name")
+    name = input("Please enter your name: ")
+
+for i in range(0, len(name), 2):
+    word 
